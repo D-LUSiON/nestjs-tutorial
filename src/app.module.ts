06ba790ce78fe5c73ff74d548ecf867b2fba3f1c
Lubomir Peikov <l.peikov@gmail.com>
2020-11-12 17:32:39 +0200

    add: TypeORM and connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './config/typeorm.config';
 import { RoomsModule } from './controllers/rooms/rooms.module';
 
 @Module({
-    imports: [RoomsModule],
+    imports: [
+        RoomsModule,
+        TypeOrmModule.forRoot(TypeOrmConfig)
+    ],
     controllers: [],
     providers: [],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const TypeOrmConfig :TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'chat',
+    username: 'postgres',
+    password: 'postgres',
+    synchronize: true,
+    entities: [
+        __dirname + '../**/*.entity.{js,ts}'
+    ]
+}
